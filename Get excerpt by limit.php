351378fdf379376b1bb00c7491586fff3e4acf77
levantoan <levantoan01@gmail.com>
2015-08-18 16:18:26 +0700

    Add excerpt by limit

---
+++ Get excerpt by limit.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Get excerpt by limit
+*/
+function excerpt($limit) {
+      $excerpt = explode(' ', get_the_excerpt(), $limit);
+      $permalink = get_the_permalink();
+      if (count($excerpt)>=$limit) {
+        array_pop($excerpt);
+        $excerpt = implode(" ",$excerpt).' <a href="'.$permalink.'">... View More</a>';
+      } else {
+        $excerpt = implode(" ",$excerpt);
+      } 
+      $excerpt = preg_replace('`\[[^\]]*\]`','',$excerpt);
+      return $excerpt;
+}
