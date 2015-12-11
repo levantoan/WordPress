a00cd290fb13ba2d7d378f4bbfdb9667a87e830e
levantoan <levantoan@users.noreply.github.com>
2015-12-11 14:50:36 +0700

    Update multi_domain.php

--- multi_domain.php
+++ multi_domain.php
@@ -3,3 +3,7 @@
 //Multi Domain for a site
 define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST']);
 define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST']);
+
+
+//Add this to head. SEo
+<link rel="canonical" href="your main doamin" />
