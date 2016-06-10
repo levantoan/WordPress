74e5e664baad464bd3b70c81e32895b4b4e4595e
levantoan <levantoan@users.noreply.github.com>
2016-06-10 17:05:56 +0700

    Update send_mail_temp.php

--- send_mail_temp.php
+++ send_mail_temp.php
@@ -1,1 +1,6 @@
-a
+/*Send Mail to customer*/
+$to = $book_email;
+$subject = 'Your book - '. get_bloginfo('name');
+$body = 'Thank you for your booking! We will contact with you soon.';
+$headers = array('Content-Type: text/html; charset=UTF-8');		 
+$sendmail = wp_mail( $to, $subject, $body, $headers );
