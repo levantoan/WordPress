0fb3e76d6040d10af6348643c7c6a7444a5fe8b6
levantoan <levantoan@users.noreply.github.com>
2016-07-09 09:53:54 +0700

    Create redriect_cpt.php

---
+++ redriect_cpt.php
@@ -0,0 +1,9 @@
+<?php
+/*Chuyển hướng Custom Post type Rooms đến page có slug rooms*/
+add_action('wp', 'redirect_ctps');
+function redirect_ctps(){ 
+    global $post;
+    if( (is_archive('rooms')) ){ 
+        wp_redirect( home_url('/rooms/') ); exit; 
+    }
+}
