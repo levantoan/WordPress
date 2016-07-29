64763aa118c00c5be24e55f384f136a28dffb376
levantoan <levantoan@users.noreply.github.com>
2016-07-29 15:45:39 +0700

    Update overview_custom_post_type_to_Page.php

--- overview_custom_post_type_to_Page.php
+++ overview_custom_post_type_to_Page.php
@@ -1,3 +1,7 @@
+/**
+ * Copy this to functions.php
+ * change "videos" to slug custom post type
+ *//
 add_action('init', function () {
      add_rewrite_rule('videos/?$','index.php?pagename=videos', 'top');
      flush_rewrite_rules();
