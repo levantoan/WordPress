b6316995c3c10a566fde0671c1d9e0f3b5973b17
levantoan <levantoan@users.noreply.github.com>
2016-10-09 20:34:37 +0700

    Update 000 More_code_helpful.php

--- 000 More_code_helpful.php
+++ 000 More_code_helpful.php
@@ -4,3 +4,16 @@ if(false === get_option("medium_crop"))
     add_option("medium_crop", "1");
 else
     update_option("medium_crop", "1");
+
+
+//Set posts_per_page in archive and more
+function hwl_home_pagesize( $query ) {
+    if ( is_admin() || ! $query->is_main_query() )
+        return;
+
+    if ( is_post_type_archive( 'thuong_hieu' ) ) {       
+        $query->set( 'posts_per_page', -1 );
+        return;
+    }
+}
+add_action( 'pre_get_posts', 'hwl_home_pagesize', 1 );
