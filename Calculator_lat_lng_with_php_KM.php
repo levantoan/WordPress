94bd33e86e77990216193df23340d92750b714b9
Lê Văn Toản <levantoan@users.noreply.github.com>
2017-01-05 11:45:35 +0700

    Create Calculator_lat_lng_with_php_KM.php

---
+++ Calculator_lat_lng_with_php_KM.php
@@ -0,0 +1,9 @@
+<?php
+function distance($lat1, $lon1, $lat2, $lon2) {
+  $theta = $lon1 - $lon2;
+  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+  $dist = acos($dist);
+  $dist = rad2deg($dist);
+  $miles = $dist * 60 * 1.1515 * 1.609344;
+  return $miles;
+}
