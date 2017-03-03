8674902b6b5c7758eafb422bdd71f40638339c36
Lê Văn Toản <levantoan@users.noreply.github.com>
2017-03-03 23:00:36 +0700

    Update same slug taxonomy and custom post type.php

--- same slug taxonomy and custom post type.php
+++ same slug taxonomy and custom post type.php
@@ -23,3 +23,29 @@ function taxonomy_slug_rewrite($wp_rewrite) {
     $wp_rewrite->rules = $rules + $wp_rewrite->rules;
 }
 add_filter('generate_rewrite_rules', 'taxonomy_slug_rewrite');
+
+/*
+base product category same base shop Page for woocommerce
+Author: http://levantoan.com/cach-cai-dat-base-cua-danh-muc-san-pham-giong-voi-base-cua-trang-san-pham/
+*/
+
+add_filter( 'rewrite_rules_array', function( $rules )
+{
+    $new_rules = array();
+    $terms = get_terms( array(
+        'taxonomy' => 'product_cat',
+        'post_type' => 'product',
+        'hide_empty' => false,
+    ));
+    if($terms && !is_wp_error($terms)){
+        $siteurl = esc_url(home_url('/'));
+        foreach ($terms as $term){
+            $term_slug = $term->slug;
+            $baseterm = str_replace($siteurl,'',get_term_link($term->term_id,'product_cat'));
+
+            $new_rules[$baseterm.'?$'] = 'index.php?product_cat='.$term_slug;
+            $new_rules[$baseterm.'page/([0-9]{1,})/?$'] = 'index.php?product_cat='.$term_slug.'&paged=$matches[1]';
+        }
+    }
+    return $new_rules + $rules;
+} );
