aa9572a1a28e25e3096fe16b15cb27c8b2b14d3c
Lê Văn Toản <levantoan@users.noreply.github.com>
2017-05-17 00:36:37 +0700

    Update all_transient.php

--- all_transient.php
+++ all_transient.php
@@ -2,7 +2,7 @@
 /*
 * set and get transient
 */
-$transientName = 'dslv_cache_query_allpost';
+$transientName = 'dvls_cache_query_allpost';
 if ( false === ( $near_store = get_transient( $transientName ) ) ) {
     $args = array(
         'post_type' => 'local-store',
@@ -20,7 +20,7 @@ add_action( 'wp_insert_post', 'devvn_delete_all_transient_khoahoc' );
 add_action( 'publish_post', 'devvn_delete_all_transient_khoahoc' );
 function devvn_delete_all_transient_khoahoc() {
     global $wpdb;
-    $menus = $wpdb->get_col( 'SELECT option_name FROM '.$wpdb->prefix.'options WHERE option_name LIKE "_transient_dslv_cache_query_%" ' );
+    $menus = $wpdb->get_col( 'SELECT option_name FROM '.$wpdb->prefix.'options WHERE option_name LIKE "_transient_dvls_cache_query_%" ' );
     foreach( $menus as $menu ) {
         $key = str_replace( '_transient_', '', $menu );
         delete_transient( $key );
