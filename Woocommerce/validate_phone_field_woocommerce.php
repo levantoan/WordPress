3ae59a3d6c39e12c415f2f651800fa0a94449454
Lê Văn Toản <levantoan@users.noreply.github.com>
2017-10-25 23:01:37 +0700

    Update validate_phone_field_woocommerce.php

--- Woocommerce/validate_phone_field_woocommerce.php
+++ Woocommerce/validate_phone_field_woocommerce.php
@@ -2,6 +2,7 @@
 add_action('woocommerce_checkout_process', 'devvn_validate_phone_field_process');
 function devvn_validate_phone_field_process() {
     $billing_phone = filter_input(INPUT_POST, 'billing_phone');
+    /*if ( ! (preg_match('/^(09|08)([0-9]{8})+$|^01([0-9]{9})+$/D', $billing_phone )) ){*/
     if ( ! (preg_match('/^0([0-9]{9,10})+$/D', $billing_phone )) ){
         wc_add_notice( "Xin nhập đúng <strong>số điện thoại</strong> của bạn"  ,'error' );
     }
