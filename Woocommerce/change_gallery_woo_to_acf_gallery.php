b9cca504cfcbba8437c2f8dba5da7a41ada6066e
Lê Văn Toản <levantoan@users.noreply.github.com>
2019-02-18 11:24:16 +0700

    Create change_gallery_woo_to_acf_gallery.php

---
+++ Woocommerce/change_gallery_woo_to_acf_gallery.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Author: levantoan.com
+*/
+
+add_action( 'wp_ajax_change_gallery_woo_to_acf', 'change_gallery_woo_to_acf_func' );
+function change_gallery_woo_to_acf_func() {
+    $all_products = new WP_Query(array(
+        'post_type' =>  'product',
+        'p' =>  224,
+    ));
+    if($all_products->have_posts()):
+        while($all_products->have_posts()):$all_products->the_post();
+            $old_gallery = get_post_meta(get_the_ID(),'_product_image_gallery', true);
+            if(!is_array($old_gallery)){
+                $t = explode(',', $old_gallery);
+                update_post_meta(get_the_ID(), '_product_image_gallery', $t);
+                echo $old_gallery . ' => ' . maybe_serialize($t) . '<br>';
+            }
+        endwhile;
+    endif; wp_reset_query();
+    die();
+}
