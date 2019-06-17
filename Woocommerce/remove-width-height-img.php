0da60856220e5a251051a9a4b27e51b32d0690ff
Lê Văn Toản <levantoan@users.noreply.github.com>
2019-06-17 22:25:32 +0700

    Update remove-width-height-img.php

--- Woocommerce/remove-width-height-img.php
+++ Woocommerce/remove-width-height-img.php
@@ -2,6 +2,7 @@
 add_filter( 'post_thumbnail_html', 'remove_wps_width_attribute', 10 );
 add_filter( 'image_send_to_editor', 'remove_wps_width_attribute', 10 );
 add_filter( 'woocommerce_product_get_image', 'remove_wps_width_attribute', 10 );
+add_filter( 'woocommerce_single_product_image_thumbnail_html', 'remove_wps_width_attribute', 10 );
 function remove_wps_width_attribute( $html ) {
     $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
     return $html;
