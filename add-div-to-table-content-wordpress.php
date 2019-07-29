a1c8b61aa6f5fcd36e16a64d0a625dfd2028bb68
Lê Văn Toản <levantoan@users.noreply.github.com>
2019-07-30 00:35:47 +0700

    Create add-div-to-table-content-wordpress.php

---
+++ add-div-to-table-content-wordpress.php
@@ -0,0 +1,9 @@
+/*
+* Author: levantoan.com
+Add div to table in wordpress content
+*/
+function devvn_table_respon($content) {
+    $content = preg_replace('/<table([\S\s]*?)<\/table>/', '<div class="devvn_table_respon"><table$1</table></div>', $content);
+    return $content;
+}
+add_filter('the_content', 'devvn_table_respon');
