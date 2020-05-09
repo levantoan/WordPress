1d8f52dbdd37b5c6c579daeee8059472903ddda4
Lê Văn Toản <levantoan@users.noreply.github.com>
2020-05-10 01:04:08 +0700

    Create 00 checkout field.php

---
+++ Woocommerce/00 checkout field.php
@@ -0,0 +1,73 @@
+<?php
+
+add_filter('woocommerce_checkout_fields', 'dms_custom_override_checkout_fields', 9999999);
+function dms_custom_override_checkout_fields($fields)
+{
+    //billing
+    $fields['billing']['billing_first_name'] = array(
+        'label' => __('Họ và tên', 'devvn'),
+        'placeholder' => _x('Họ và tên', 'placeholder', 'devvn'),
+        'required' => true,
+        'class' => array('form-row-first'),
+        'clear' => true,
+        'priority' => 10
+    );
+    unset($fields['billing']['billing_last_name']);
+    unset($fields['billing']['billing_company']);
+    unset($fields['billing']['billing_country']);
+    unset($fields['billing']['billing_postcode']);
+    unset($fields['billing']['billing_state']);
+    unset($fields['billing']['billing_city']);
+    unset($fields['billing']['billing_address_2']);
+    $fields['billing']['billing_phone']['priority'] = 20;
+    $fields['billing']['billing_phone']['class'] = array('form-row-last');
+    $fields['billing']['billing_phone']['placeholder'] = _x('Số điện thoại', 'placeholder', 'devvn');
+    $fields['billing']['billing_address_1']['class'] = array('form-row-wide');
+    $fields['billing']['billing_address_1']['priority'] = 22;
+
+
+    $fields['billing']['billing_email']['priority'] = 25;
+    $fields['billing']['billing_email']['class'] = array('form-row-wide');
+    $fields['billing']['billing_email']['required'] = false;
+
+    //shipping
+    $fields['shipping']['shipping_first_name'] = array(
+        'label' => __('Họ và tên', 'devvn'),
+        'placeholder' => _x('Họ và tên', 'placeholder', 'devvn'),
+        'required' => true,
+        'class' => array('form-row-first'),
+        'clear' => true,
+        'priority' => 10
+    );
+    unset($fields['shipping']['shipping_last_name']);
+    unset($fields['shipping']['shipping_company']);
+    unset($fields['shipping']['shipping_country']);
+    unset($fields['shipping']['shipping_state']);
+    unset($fields['shipping']['shipping_postcode']);
+    unset($fields['shipping']['shipping_city']);
+    unset($fields['shipping']['shipping_address_2']);
+    $fields['shipping']['shipping_address_1']['class'] = array('form-row-wide');
+    $fields['shipping']['shipping_phone'] = array(
+        'label' => __('Số điện thoại', 'devvn'),
+        'placeholder' => _x('Số điện thoại', 'placeholder', 'devvn'),
+        'required' => true,
+        'class' => array('form-row-last'),
+        'clear' => true,
+        'priority'  =>  20
+    );
+
+    uasort($fields['billing'], 'dms_sort_fields_by_order');
+    uasort($fields['shipping'], 'dms_sort_fields_by_order');
+
+    return $fields;
+}
+
+if(!function_exists('dms_sort_fields_by_order')) {
+    function dms_sort_fields_by_order($a, $b)
+    {
+        if (!isset($b['priority']) || !isset($a['priority']) || $a['priority'] == $b['priority']) {
+            return 0;
+        }
+        return ($a['priority'] < $b['priority']) ? -1 : 1;
+    }
+}
