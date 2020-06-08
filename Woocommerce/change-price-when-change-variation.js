2f59175c0c22b021dc98cfeb5112966156ceca06
Lê Văn Toản <levantoan@users.noreply.github.com>
2020-06-08 20:14:14 +0700

    Update change-price-when-change-variation.js

--- Woocommerce/change-price-when-change-variation.js
+++ Woocommerce/change-price-when-change-variation.js
@@ -11,40 +11,7 @@ Add this CSS to your style.css
 
 (function ($) {
     $(document).ready(function () {
-        var old_price = $('.summary.entry-summary > .price-wrapper > .price');
-        var old_price_val = old_price.html();
-        $('.variations_form.cart').each(function () {
-            var thisFormOld = $(this);
-            $(this).on('check_variations', function () {
-                var thisForm = $(this);
-                var variation_wrap = $('.woocommerce-variation.single_variation', thisForm);
-                var sync_price = $('.single_variation_wrap div.woocommerce-variation > .woocommerce-variation-price > .price', thisForm);
-                if (variation_wrap.is(':visible')) {
-                    old_price.html(sync_price.html());
-                } else {
-                    old_price.html(old_price_val);
-                }
-                setTimeout( function() {
-                    var thisNoMatching = $('.wc-no-matching-variations', thisForm).length;
-                    if(thisNoMatching) {
-                        old_price.html(old_price_val);
-                    }
-                }, 201 );
-            });
-
-            $('select', this).on('change', function () {
-                thisFormOld.trigger('check_variations');
-            });
-        });
-    });
-})(jQuery);
-
-
-//Code thứ 2 nếu code trên ko được
-
-(function($){
-    $(document).ready(function () {
-		if($('.product-info.summary').length > 0) {
+        if ($('.product-info.summary').length > 0) {
             $('.product-info.summary').each(function () {
                 var oldPriceWrap = $('.price-wrapper', this);
                 var thisOldPrice = oldPriceWrap.html();
@@ -52,18 +19,22 @@ Add this CSS to your style.css
                     var thisVal = $(this).val();
                     var thisForm = $(this).closest('.variations_form.cart');
                     var thisVariation = thisForm.data('product_variations');
-                    clearNotice();
+                    var have_variation_price = false;
                     if (thisVal) {
                         $(thisVariation).each(function (index, value) {
-                           if(value.variation_id == thisVal) {
-                               oldPriceWrap.html(value.price_html);
-                           }
+                            if (value.variation_id == thisVal && value.price_html) {
+                                oldPriceWrap.html(value.price_html);
+                                have_variation_price = true;
+                            }
                         });
-                    }else{
+                        if(!have_variation_price){
+                            oldPriceWrap.html(thisOldPrice)
+                        }
+                    } else {
                         oldPriceWrap.html(thisOldPrice);
                     }
                 });
             });
         }
-	});
+    });
 })(jQuery);
