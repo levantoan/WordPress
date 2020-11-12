211cc2f522d70d0acf1215f25ee14a5b16acbfb6
Lê Văn Toản <levantoan@users.noreply.github.com>
2020-11-13 03:07:58 +0700

    Create yoast-seo-custom-field.php

---
+++ yoast-seo-custom-field.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Yoast Seo Custom Field
+*/
+function devvn_get_name_term($term_id = ''){
+    if($term_id && is_numeric($term_id)){
+        $term = get_term_by('id', $term_id, 'escorts');
+        if($term && !is_wp_error($term)){
+            return $term->name;
+        }
+    }
+    return $term_id;
+}
+
+function filter_wpseo_replacements( $replacements ) {
+    global $gender_a, $ethnicity_a;
+    if( isset( $replacements['%%cf_country%%'] ) ){
+        $replacements['%%cf_country%%'] = devvn_get_name_term($replacements['%%cf_country%%']);
+    }
+    if( isset( $replacements['%%cf_state%%'] ) ){
+        $replacements['%%cf_state%%'] = devvn_get_name_term($replacements['%%cf_state%%']);
+    }
+    if( isset( $replacements['%%cf_city%%'] ) ){
+        $replacements['%%cf_city%%'] = devvn_get_name_term($replacements['%%cf_city%%']);
+    }
+    if( isset( $replacements['%%cf_ethnicity%%'] ) ){
+        $replacements['%%cf_ethnicity%%'] = $ethnicity_a[$replacements['%%cf_ethnicity%%']];
+    }
+    if( isset( $replacements['%%cf_gender%%'] ) ){
+        $replacements['%%cf_gender%%'] = $gender_a[$replacements['%%cf_gender%%']];
+    }
+    return $replacements;
+};
+add_filter( 'wpseo_replacements', 'filter_wpseo_replacements', 10, 1 );
