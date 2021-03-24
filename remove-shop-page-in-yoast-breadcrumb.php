962b9b60a2e1c3888b56f150cb7aa98671a64a94
Lê Văn Toản <levantoan@users.noreply.github.com>
2021-03-24 07:20:03 +0700

    Update remove-shop-page-in-yoast-breadcrumb.php

--- remove-shop-page-in-yoast-breadcrumb.php
+++ remove-shop-page-in-yoast-breadcrumb.php
@@ -6,7 +6,7 @@ Author: https://levantoan.com
 
 function devvn_wpseo_breadcrumb_output( $output ){
     if( is_product() ){
-        $from = '<a href="'.home_url('/shop/').'" >Sản phẩm</a> <span class="divider">»</span>';
+        $from = '<a href="'.home_url('/shop/').'">Sản phẩm</a> <span class="divider">»</span>';
         $to     = '';
         $output = str_replace( $from, $to, $output );
     }
