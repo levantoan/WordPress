1b42ab8466e5664c1ad17f600e75836917e72322
Lê Văn Toản <levantoan@users.noreply.github.com>
2021-04-07 11:44:19 +0700

    Update 0đ to Liên hệ trong cart và checkout.php

--- Woocommerce/0đ to Liên hệ trong cart và checkout.php
+++ Woocommerce/0đ to Liên hệ trong cart và checkout.php
@@ -28,3 +28,17 @@ function devvn_woocommerce_get_order_item_totals($total_rows){
     }
     return $total_rows;
 }
+
+function devvn_wc_custom_get_price_html( $price, $product ) {
+    if ( $product->get_price() == 0 ) {
+        if ( $product->is_on_sale() && $product->get_regular_price() ) {
+            $regular_price = wc_get_price_to_display( $product, array( 'qty' => 1, 'price' => $product->get_regular_price() ) );
+ 
+            $price = wc_format_price_range( $regular_price, __( 'Free!', 'woocommerce' ) );
+        } else {
+            $price = '<span class="amount">' . __( 'Liên hệ', 'woocommerce' ) . '</span>';
+        }
+    }
+    return $price;
+}
+add_filter( 'woocommerce_get_price_html', 'devvn_wc_custom_get_price_html', 10, 2 );
