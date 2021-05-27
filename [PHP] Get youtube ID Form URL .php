de8e46861d97d238c558f038d03388baeee4067e
Lê Văn Toản <levantoan@users.noreply.github.com>
2021-05-27 21:42:32 +0700

    Update [PHP] Get youtube ID Form URL .php

--- [PHP] Get youtube ID Form URL .php
+++ [PHP] Get youtube ID Form URL .php
@@ -1,3 +1,41 @@
+Cách 1
+
+function getYouTubeVideoId($url)
+{
+    $video_id = false;
+    $url = parse_url($url);
+    if (strcasecmp($url['host'], 'youtu.be') === 0)
+    {
+        #### (dontcare)://youtu.be/<video id>
+        $video_id = substr($url['path'], 1);
+    }
+    elseif (strcasecmp($url['host'], 'www.youtube.com') === 0)
+    {
+        if (isset($url['query']))
+        {
+            parse_str($url['query'], $url['query']);
+            if (isset($url['query']['v']))
+            {
+                #### (dontcare)://www.youtube.com/(dontcare)?v=<video id>
+                $video_id = $url['query']['v'];
+            }
+        }
+        if ($video_id == false)
+        {
+            $url['path'] = explode('/', substr($url['path'], 1));
+            if (in_array($url['path'][0], array('e', 'embed', 'v')))
+            {
+                #### (dontcare)://www.youtube.com/(whitelist)/<video id>
+                $video_id = $url['path'][1];
+            }
+        }
+    }else{
+        return false;
+    }
+    return $video_id;
+}
+
+Cách 2
 /*
  * http://stackoverflow.com/questions/5830387/how-to-find-all-youtube-video-ids-in-a-string-using-a-regex
  */
