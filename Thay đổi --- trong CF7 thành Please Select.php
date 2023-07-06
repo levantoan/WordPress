512c30356cb469bdce237eea3fa145b7b53c3760
Lê Văn Toản <levantoan@users.noreply.github.com>
2023-07-06 20:43:53 +0700

    Update Thay đổi --- trong CF7 thành Please Select.php

--- Thay đổi --- trong CF7 thành Please Select.php
+++ Thay đổi --- trong CF7 thành Please Select.php
@@ -1,4 +1,35 @@
 <?php
+/*new version*/
+function devvn_replace_include_blank($name, $text, &$html) {
+    $matches = false;
+    preg_match('/<select(.*)name="' . $name . '"[^>]*>(.*)<\/select>/iU', $html, $matches);
+    if ($matches) {
+        $select = str_replace('<option value="">&#8212;Please choose an option&#8212;</option>', '<option value="">' . $text . '</option>', $matches[0]);
+        $html = preg_replace('/<select(.*)name="' . $name . '"[^>]*>(.*)<\/select>/iU', $select, $html);
+    }
+}
+
+function devvn_wpcf7_form_elements($html) {
+    devvn_replace_include_blank('cus_gender', 'Title (Mr, Mrs)', $html);
+    devvn_replace_include_blank('booking_adult', 'Adult (from 12 years)', $html);
+    devvn_replace_include_blank('booking_children', 'Children ( 2 - 12 years )', $html);
+    devvn_replace_include_blank('booking_infant', 'Infant ( <2 years )', $html);
+
+    return $html;
+}
+add_filter('wpcf7_form_elements', 'devvn_wpcf7_form_elements');
+
+
+
+
+
+
+
+
+
+
+
+/*For old version - đã loại bỏ không dùng nữa*/
 function my_wpcf7_form_elements($html) {
 	function ov3rfly_replace_include_blank($name, $text, &$html) {
 		$matches = false;
