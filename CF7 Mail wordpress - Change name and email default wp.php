eb88f74d4b2efac04f85013125870c9ab9912aad
Lê Văn Toản <levantoan@users.noreply.github.com>
2024-03-19 00:47:11 +0700

    Update CF7 Mail wordpress - Change name and email default wp.php

--- CF7 Mail wordpress - Change name and email default wp.php
+++ CF7 Mail wordpress - Change name and email default wp.php
@@ -1,10 +1,16 @@
 <?php
 add_filter('wp_mail_from', 'new_mail_from');
-add_filter('wp_mail_from_name', 'new_mail_from_name');
 function new_mail_from($old) {
-	//get_option('admin_email')
-	return 'your-email@your-domain.com';
+    if (strpos($old, 'wordpress@') !== false) {
+        $old = get_option('admin_email');
+    }
+    return $old;
 }
+
+add_filter('wp_mail_from_name', 'new_mail_from_name');
 function new_mail_from_name($old) {
- return 'TÊN MONG MUỐN';
+    if($old == 'WordPress'){
+        $old = get_option('blogname');
+    }
+    return $old;
 }
