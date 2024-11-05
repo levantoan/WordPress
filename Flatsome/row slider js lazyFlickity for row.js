6905c3dad7f0f98809ff07e6a574eaaf3765a4ee
Lê Văn Toản <levantoan@users.noreply.github.com>
2024-11-05 21:25:51 +0700

    Update row slider js lazyFlickity for row.js

--- Flatsome/row slider js lazyFlickity for row.js
+++ Flatsome/row slider js lazyFlickity for row.js
@@ -13,3 +13,32 @@ if($('.devvn_slider_prod').length){
         })
     });
 }
+
+/*php
+
+add_action('wp_footer', function(){
+    ?>
+    <script>
+        (function ($){
+        	$(document).ready(function() {
+        		$('.row-slider-image-box').each(function(){
+        			$(this).find('style[scope="scope"]').remove();
+        			$(this).lazyFlickity({
+            			"cellAlign": "left",
+                          "wrapAround": false,
+                          "autoPlay": false,
+                          "prevNextButtons": true,
+                          "percentPosition": true,
+                          "imagesLoaded": true,
+                          "pageDots": true,
+                          "rightToLeft": false,
+                          "contain": true,
+                          lazyLoad: 1,
+        			});
+        		});
+        	});
+        })(jQuery);
+        </script>
+    <?php
+});
+*/
